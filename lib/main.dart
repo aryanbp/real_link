22c7e0d72dcf20440e251908f5b80dd690b2153f
Aryan Beenu Panikar <aryanpanikar9@gmail.com>
2023-01-26 17:57:27 +0530

    Add Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'dart:async';
+
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'package:flutter/material.dart';
 
 import 'me.dart';
 
-
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,9 +26,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Link App',
       theme: ThemeData(
-        textTheme: const TextTheme(  bodyText2: TextStyle(color: Colors.white),),
+        textTheme: GoogleFonts.ubuntuTextTheme(
+          const TextTheme(
+            bodyText2: TextStyle(color: Colors.white),
+          ),
+        ),
         primarySwatch: Colors.purple,
-        scaffoldBackgroundColor:Colors.black,
+        scaffoldBackgroundColor: Colors.black,
       ),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Home Page'),
@@ -37,43 +50,215 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-      if (index==2){
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const Me(title: 'Me')),
-        );
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      var user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const ThirdScreen()));
+      } else {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const SecondScreen()));
       }
-
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo.png'),
+            const Text('Real_Link',
+                style: TextStyle(color: Colors.purple, fontSize: 50)),
+          ],
+        ),
+      ),
+      // color: Colors.black,
+      // child: Icon(
+      //   Icons.connect_without_contact_sharp,
+      //   color: Colors.deepPurple,
+      //   size: 300,
+      // )
+      //FlutterLogo(size:MediaQuery.of(context).size.height,textColor: Colors.purple,)
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key});
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  final mycon = TextEditingController(text: '+91 7045614665');
+
+  String textholde = "Enter Your Phone No:";
+  String smsCode = '';
+  String verf = '';
+  int val = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
+        title: Text(
+          'Login Page',
+          style: GoogleFonts.ubuntu(),
+        ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(textholde, style: const TextStyle(fontSize: 20)),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.purpleAccent),
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white, fontSize: 20),
+                  autofocus: true,
+                  textAlign: TextAlign.center,
+                  controller: mycon,
+                ),
+              ),
+            ),
+            TextButton(
+                onPressed: () async {
+                  FirebaseAuth auth = FirebaseAuth.instance;
+                  if (val == 0) {
+                    val = 1;
+                    setState(() {
+                      textholde = 'Authenticating...';
+                    });
+                    await FirebaseAuth.instance.verifyPhoneNumber(
+                      phoneNumber: mycon.text,
+                      verificationCompleted:
+                          (PhoneAuthCredential credential) async {
+                        // ANDROID ONLY!
+
+                        // Sign the user in (or link) with the auto-generated credential
+                        //await auth.signInWithCredential(credential);
+                      },
+                      verificationFailed: (FirebaseAuthException e) {
+                        if (e.code == 'invalid-phone-number') {
+                          setState(() {
+                            textholde =
+                                'The provided phone number is not valid.';
+                          });
+                        }
+
+                        // Handle other errors
+                      },
+                      codeSent:
+                          (String verificationId, int? resendToken) async {
+                        // Update the UI - wait for the user to enter the SMS code
+                        setState(() {
+                          textholde = 'Enter OTP: ';
+                        });
+                        mycon.clear();
+                        verf = verificationId;
+
+                        // Create a PhoneAuthCredential with the code
+                      },
+                      codeAutoRetrievalTimeout: (String verificationId) {
+                        // Auto-resolution timed out...
+                      },
+                    );
+                  } else {
+                    val = 0;
+                    smsCode = mycon.text;
+                    PhoneAuthCredential credential =
+                        PhoneAuthProvider.credential(
+                            verificationId: verf, smsCode: smsCode);
+                    // Sign the user in (or link) with the credential
+                    await auth.signInWithCredential(credential);
+                    setState(() {
+                      //textholde = 'Done!!!! ';
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ThirdScreen()));
+                    });
+                  }
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purpleAccent),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(50),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          'Click Me',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      )),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdScreen extends StatefulWidget {
+  const ThirdScreen({super.key});
+
+  @override
+  State<ThirdScreen> createState() => _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if (index == 2) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Me(title: 'Me')),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Home Page',
+          style: GoogleFonts.ubuntu(),
+        ),
+      ),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
             Text(
-              'Go to Login:',
+              'Welcome Home:',
             ),
           ],
         ),
@@ -82,15 +267,24 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.black,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home,color: Colors.deepPurpleAccent,),
+            icon: Icon(
+              Icons.home,
+              color: Colors.deepPurpleAccent,
+            ),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business,color: Colors.deepPurpleAccent,),
+            icon: Icon(
+              Icons.business,
+              color: Colors.deepPurpleAccent,
+            ),
             label: 'Business',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person,color: Colors.deepPurpleAccent,),
+            icon: Icon(
+              Icons.person,
+              color: Colors.deepPurpleAccent,
+            ),
             label: ' Me',
           ),
         ],
--- lib/me.dart
+++ lib/me.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:draggable_fab/draggable_fab.dart';
+
+import 'main.dart';
 
 class Me extends StatelessWidget {
   const Me({super.key, required this.title});
@@ -12,9 +16,9 @@ class Me extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             Container(
-              margin: EdgeInsets.all(30),
+              margin: EdgeInsets.only(left: 30, right: 30, top: 30, bottom: 0),
               decoration: const BoxDecoration(
                 borderRadius: BorderRadius.all(Radius.circular(50)),
                 color: Colors.white,
@@ -26,36 +30,223 @@ class Me extends StatelessWidget {
                     margin: EdgeInsets.all(20),
                     height: 50,
                     width: 50,
-                    decoration:BoxDecoration(
+                    decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(50)),
                       color: Colors.black,
                     ),
-                    child:Icon(Icons.person,color: Colors.white,),
-                  ),
-                  Container(
-                    child: Column(
-                      children: [
-                        Text("Umar Shaikh",style: TextStyle(color: Colors.black),),
-                        Text("4 Devices",style: TextStyle(color: Colors.grey,fontSize: 11),),
-                      ],
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
                     ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "Umar Shaikh",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Text(
+                        "4 Devices",
+                        style: TextStyle(color: Colors.grey, fontSize: 11),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
+            Container(
+              child: Container(
+                margin: EdgeInsets.all(30),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 20, bottom: 20, right: 15, left: 20),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.message,
+                              color: Colors.orangeAccent,
+                              size: 40,
+                            ),
+                            Text(
+                              'Notification',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 20, bottom: 20, right: 15, left: 15),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.note_alt_sharp,
+                              color: Colors.green,
+                              size: 40,
+                            ),
+                            Text(
+                              'Orders',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 20, bottom: 20, left: 15, right: 15),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.question_answer,
+                              color: Colors.purple,
+                              size: 40,
+                            ),
+                            Text(
+                              'Features',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Container(
+                margin: EdgeInsets.all(30),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 8.0),
+                                    child: Icon(
+                                      Icons.arrow_circle_up,
+                                      color: Colors.black,
+                                      size: 40,
+                                    ),
+                                  ),
+                                  Text('Firmwareupdate',style: TextStyle(color: Colors.black),),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 70.0),
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Container(
+                margin: EdgeInsets.all(30),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 8.0),
+                                    child: Icon(
+                                      Icons.keyboard_voice,
+                                      color: Colors.black,
+                                      size: 40,
+                                    ),
+                                  ),
+                                  Text('Voice Service',style: TextStyle(color: Colors.black),),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 70.0),
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             const Text(
-              'Welcome Home.',
+              'Your Info',
             ),
           ],
         ),
       ),
-      floatingActionButton: DraggableFab(
-        child: FloatingActionButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Icon(Icons.arrow_left),
-        ),
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Spacer(),
+          Spacer(),
+      FloatingActionButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyHomePage(
+                              title: 'Splash Page',
+                            )));
+              },
+              child: const Icon(Icons.logout),
+            ),
+          Spacer(),
+      FloatingActionButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Icon(Icons.arrow_left),
+            ),
+          Spacer(),
+        ],
       ),
     );
   }
